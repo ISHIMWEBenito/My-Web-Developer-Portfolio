3c0cff8ec01d743711afbbbe88550bfb7c391434
BenitoIshimwe <benishimwe31@gmail.com>
2023-02-22 13:35:05 +0100

    Add new project

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,4 +1,4 @@
-import './projects.css';
+import "./projects.css";
 
 const Members = () => {
   return (
@@ -6,25 +6,29 @@ const Members = () => {
       <h1 className='member-txt'>My Projects</h1>
 
       <div className='member member-6'>
-        <div className='member-img6'></div>
+        <a href='https://benish.netlify.app/' className='contact-member'>
+          <div className='member-img6'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>My First Portfolio</h1>
           <h3 className='position'>HTML CSS</h3>
           <h4 className='about'>Learn more about my portfolio</h4>
-          <a
-            href='https://benish.netlify.app/'
-            className='contact-member'
-          >
+          <a href='https://benish.netlify.app/' className='contact-member'>
             <span>Learn More...</span>
           </a>
         </div>
       </div>
 
       <div className='member member-4'>
-        <div className='member-img4'></div>
+        <a
+          href='https://adventurousworld.netlify.app/'
+          className='contact-member'
+        >
+          <div className='member-img4'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>Adventurous World</h1>
-          <h3 className='position'>React Native</h3>
+          <h3 className='position'>React</h3>
           <h4 className='about'>Learn more about my project</h4>
           <a
             href='https://adventurousworld.netlify.app/'
@@ -36,7 +40,12 @@ const Members = () => {
       </div>
 
       <div className='member member-2'>
-        <div className='member-img2'></div>
+        <a
+          href='https://benitoinhiskitchen.netlify.app/'
+          className='contact-member'
+        >
+          <div className='member-img2'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>Restaurant Website</h1>
           <h3 className='position'>React</h3>
@@ -51,7 +60,12 @@ const Members = () => {
       </div>
 
       <div className='member member-5'>
-        <div className='member-img5'></div>
+        <a
+          href='https://benitojuniordeveloper.herokuapp.com/'
+          className='contact-member'
+        >
+          <div className='member-img5'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>Product Add App</h1>
           <h3 className='position'>PHP MYSQL</h3>
@@ -66,24 +80,57 @@ const Members = () => {
       </div>
 
       <div className='member member-1'>
-        <div className='member-img1'></div>
+        <a
+          href='https://mern-task-app-a7vy.onrender.com'
+          className='contact-member'
+        >
+          <div className='member-img1'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>Full Stack MERN TO DO lIST APP</h1>
           <h3 className='position'>React NodeJs MongoDB</h3>
           <h4 className='about'>Learn more about my project</h4>
-          <a href='#contact' className='contact-member'>
+          <a
+            href='https://mern-task-app-a7vy.onrender.com'
+            className='contact-member'
+          >
             <span>Learn More...</span>
           </a>
         </div>
       </div>
 
       <div className='member member-3'>
-        <div className='member-img3'></div>
+        <a
+          href='https://todo-application-u1oq.onrender.com'
+          className='contact-member'
+        >
+          <div className='member-img3'></div>
+        </a>
         <div className='member-info'>
           <h1 className='name'>MEAN STACK TO-DO lIST APP</h1>
           <h3 className='position'>Angular NodeJs MongoDB</h3>
           <h4 className='about'>Learn more about my project</h4>
-          <a href='#contact' className='contact-member'>
+          <a
+            href='https://todo-application-u1oq.onrender.com'
+            className='contact-member'
+          >
+            <span>Learn More...</span>
+          </a>
+        </div>
+      </div>
+
+      <div className='member member-7'>
+        <a href='https://foodmine-vtiv.onrender.com' className='contact-member'>
+          <div className='member-img7'></div>
+        </a>
+        <div className='member-info'>
+          <h1 className='name'>FOOD MINE APP</h1>
+          <h3 className='position'>Angular NodeJs MongoDB</h3>
+          <h4 className='about'>Learn more about my project</h4>
+          <a
+            href='https://foodmine-vtiv.onrender.com'
+            className='contact-member'
+          >
             <span>Learn More...</span>
           </a>
         </div>
